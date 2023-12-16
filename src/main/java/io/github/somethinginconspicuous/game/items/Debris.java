6987141c4d6afeae99f8139d1d3f842b13dc0732
JollySeason <154002969+JollySeason@users.noreply.github.com>
2023-12-16 15:18:37 -0500

    actual code

---
+++ src/main/java/io/github/somethinginconspicuous/game/items/Debris.java
@@ -0,0 +1,33 @@
+package io.github.somethinginconspicuous.game.items;
+
+import javax.swing.ImageIcon;
+
+import io.github.somethinginconspicuous.game.Item;
+
+public class Debris implements Item {
+
+    private static Debris instance;
+
+    public static Debris getInstance(){
+        if(instance == null)
+            instance = new Debris();
+
+        return instance;
+    }
+
+    @Override
+    public ImageIcon getImageIcon() {
+        return new ImageIcon("src/main/resources/images/debris_hack.png");
+    }
+
+    @Override
+    public String name() {
+        return "Debris.";
+    }
+
+    @Override
+    public String flavor() {
+        return "It's hefty, but looks like it could break at any moment.";
+    }
+    
+}
