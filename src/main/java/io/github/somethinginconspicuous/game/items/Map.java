aa6ff9ca4b947f322b1dfa1f992cef649574495c
JollySeason <154002969+JollySeason@users.noreply.github.com>
2023-12-16 15:50:26 -0500

    Create Map.java

---
+++ src/main/java/io/github/somethinginconspicuous/game/items/Map.java
@@ -0,0 +1,33 @@
+package io.github.somethinginconspicuous.game.items;
+
+import javax.swing.ImageIcon;
+
+import io.github.somethinginconspicuous.game.Item;
+
+public class Map implements Item {
+
+    private static Map instance;
+
+    public static Map getInstance() {
+        if (instance == null)
+            instance = new Map();
+
+        return instance;
+    }
+
+    @Override
+    public ImageIcon getImageIcon() {
+        return new ImageIcon("src/main/resources/images/pxil-frame-0.png");
+    }
+
+    @Override
+    public String name() {
+        return "Blood-stained Map";
+    }
+
+    @Override
+    public String flavor() {
+        return "A dusty, stained map. There are 4 yellow dots scattered across it.";
+    }
+
+}
