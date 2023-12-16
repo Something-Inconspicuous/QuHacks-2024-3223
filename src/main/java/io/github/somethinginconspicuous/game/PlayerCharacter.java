df681e8254fe78c73a455fba56fca3b8a5f9464c
Matthew <117416222+Something-Inconspicuous@users.noreply.github.com>
2023-12-16 10:56:22 -0500

    Add TimeLimit

---
+++ src/main/java/io/github/somethinginconspicuous/game/PlayerCharacter.java
@@ -0,0 +1,9 @@
+package io.github.somethinginconspicuous.game;
+
+public class PlayerCharacter {
+    private String name;
+
+    public String name(){
+        return name;
+    }
+}
---
+++ src/main/java/io/github/somethinginconspicuous/game/TimeLimit.java
@@ -0,0 +1,37 @@
+package io.github.somethinginconspicuous.game;
+
+public class TimeLimit {
+    private int minutesLeft;
+
+    public TimeLimit(){
+        this(9 * 60); // 9 hours
+    }
+
+    public TimeLimit(int minutesLeft){
+        this.minutesLeft = minutesLeft;
+    }
+
+    public void spend(int minutes){
+        minutesLeft -= minutes;
+    }
+
+    public void spendHours(int hours){
+        spend(hours * 60);
+    }
+
+    public boolean hasTimeLeft(){
+        return minutesLeft > 0;
+    }
+
+    public int minutesLeft(){
+        return minutesLeft;
+    }
+
+    public int hoursLeft(){
+        return minutesLeft / 60;
+    }
+
+    public int minutesOnlyLeft(){
+        return minutesLeft % 60;
+    }
+}
